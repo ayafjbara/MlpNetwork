6322fa652afd5dbd78e476d5153c86a2ae88d152
Aya <aya.jbara@mail.huji.ac.il>
2019-12-15 16:42:55 +0200

    Initial commit

---
+++ Activation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aya on 12/15/2019.
+//
+
+#include "Activation.h"
---
+++ Dense.h
@@ -0,0 +1,14 @@
+//
+// Created by Aya on 12/15/2019.
+//
+
+#ifndef C___DENSE_H
+#define C___DENSE_H
+
+
+class Dense {
+
+};
+
+
+#endif //C___DENSE_H
